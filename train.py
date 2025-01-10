894c012ad60ccd5fd8c0c03bc20e6c09f51fd5fd
Gelinzh <Gelin_zh@outlook.com>
2025-01-10 17:47:03 +0800

    1-10-remote

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 from torch import nn, optim, Tensor
@@ -6,48 +7,76 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 
 from .models import ModelManager
 from .data import DataLoaderManager
-from .utils import CommandDetails, IndexLogger
+from .utils import CommandDetails, IndexLogger, Recorder
 
 class Trainer():
-    def __init__(self, cd: CommandDetails, model_mng: ModelManager, data_mng: DataLoaderManager, rank: int):
+    def __init__(self, rank: int, cd: CommandDetails, model_mng: ModelManager, data_mng: DataLoaderManager, logger: IndexLogger):
+        torch.cuda.set_device(cd.world[rank])
+        dist.init_process_group("nccl", rank=rank, world_size=len(cd.world))
+        self.rank = rank
         self.cd = cd
         self.model_mng = model_mng
         self.data_mng = data_mng
-        self.rank = rank
+        self.logger = logger
         
     def main_work(self):
-        train_dataloader = self.data_mng.get_dataloader("train", self.rank)
-        val_dataloader = self.data_mng.get_dataloader("val", self.rank)
+        train_dataloader = self.data_mng.get_dataloader("train", rank=self.rank)
+        val_dataloader = self.data_mng.get_dataloader("val", rank=self.rank)
         
-        model = self.model_mng.build_model()
-        ddp_model = DDP(model, device_ids=[self.rank])
+        model = self.model_mng.build_model().to(torch.cuda.current_device())
+        ddp_model = DDP(model, device_ids=[torch.cuda.current_device()])
         self.criterion: nn.Module = self.cd.criterion()
         self.optimizer: optim.Optimizer = self.cd.optimizer(model.parameters(), self.cd.learn_rate)
         
         for epoch in range(self.cd.epochs):
-            self.train_epoch(ddp_model, train_dataloader)
+            self.logger.start()
+            
+            if self.cd.task == "classify":
+                index = IndexLogger.ClassifyIndex(epoch=epoch, time=self.logger.get_time())
+            elif self.cd.task == "detect":
+                index = IndexLogger.DetectIndex(epoch=epoch, time=self.logger.get_time())
+            
+            # learn & val
+            self.train_epoch(ddp_model, train_dataloader, index)
             dist.barrier()
-            self.val_epoch(ddp_model, val_dataloader)
+            self.val_epoch(ddp_model, val_dataloader, index)
+            
+            self.logger(index)
     
-    def train_epoch(self, model: DDP, dataloader: DataLoader):
+    def train_epoch(self, model: DDP, dataloader: DataLoader, index: IndexLogger.Index):
         model.train()
+        losses = []
         for x, label in dataloader:
             output = model(x)
             self.optimizer.zero_grad()
             loss: Tensor = self.criterion(output, label)
             loss.backward()
             self.optimizer.step()
+            losses.append(loss.item())
+        mean_loss = Recorder.converge_loss(losses)
+        
+        # let one of the gpu fill the blanks
+        if dist.get_rank() == 0:
+            index.record_train(mean_loss)
+        dist.barrier()  # wait till it's done
 
-    def val_epoch(self, model: nn.Module, dataloader: DataLoader):
+    def val_epoch(self, model: nn.Module, dataloader: DataLoader, index: IndexLogger.Index):
         model.eval()
+        recorder = Recorder(self.model_mng.model_desc.get("nc"))
         for x, label in dataloader:
             output = model(x)
             loss = self.criterion(output, label)
+            recorder(output, label, loss)
+        recorder.converge()
+        
+        # let one of the gpu fill the blanks
+        if dist.get_rank() == 0:
+            index.record_val(recorder)
+        dist.barrier()  # wait till it's done
             
     @staticmethod
-    def init_trainer(rank, cd: CommandDetails, model_mng: ModelManager, data_mng: DataLoaderManager):
-        dist.init_process_group("nccl", rank=rank, world_size=len(cd.world))
-        trainer = Trainer(cd, model_mng, data_mng, rank)
+    def init_trainer(rank, cd: CommandDetails, model_mng: ModelManager, data_mng: DataLoaderManager, logger: IndexLogger):
+        trainer = Trainer(rank, cd, model_mng, data_mng, logger)
         trainer.main_work()
 
     
@@ -59,4 +88,4 @@ class TrainerManager():
         self.cd = cd
         
     def train(self):
-        mp.spawn(Trainer.init_trainer, args=(self.cd, self.model_mng, self.data_mng), nprocs=len(self.cd.world), join=True)
+        mp.spawn(Trainer.init_trainer, args=(self.cd, self.model_mng, self.data_mng, self.logger), nprocs=len(self.cd.world), join=True)
--- utils/stat.py
+++ utils/stat.py
@@ -1,14 +1,15 @@
 import time
 from typing import List, Dict, Any
 
-import numpy as np
+import torch
+import torch.distributed as dist
 from torch import Tensor
 
 from .cmd import CommandDetails
 
 class Calculate:
     @staticmethod
-    def update_conf_mat(conf_mat: np.ndarray, output: Tensor, label: Tensor):
+    def update_conf_mat(conf_mat: Tensor, output: Tensor, label: Tensor):
         _, pred = output.max(1)  # Get Pred Tag
         for index in range(conf_mat.shape[0]):
             conf_mat[label[index], pred[index]] += 1
@@ -16,105 +17,159 @@ class Calculate:
     @staticmethod
     def topk_accuracy(output: Tensor, label: Tensor, k: int = 5):
         _, topk_indices = output.topk(k)  # Get top-k [[top1, top2, ..., topk],...]
-        correct = topk_indices.eq(label.unsqueeze(-1))  # Get top-k correct [[False, True, ..., False], ...]
+        correct: Tensor = topk_indices.eq(label.unsqueeze(-1))  # Get top-k correct [[False, True, ..., False], ...]
         return correct.sum().item() / label.size(0)
     
     @staticmethod
-    def top1_accuracy(conf_mat: np.ndarray):
+    def top1_accuracy(conf_mat: Tensor):
         '''
-        This method is used by confusion matrix,
+        This method should be used with confusion matrix,
         for calculate top1-acc in dataloader, using topk_accuracy(..., k=1)
         '''
         TP_all = sum(conf_mat[i, i] for i in range(conf_mat.shape[0]))
-        return TP_all / np.sum(conf_mat)
+        TP_all = conf_mat.diagonal().sum()
+        return TP_all / conf_mat.sum()
 
     @staticmethod
-    def precision(conf_mat: np.ndarray):
-        res = np.zeros(conf_mat.shape[0], dtype=np.float32)
+    def precision(conf_mat: Tensor):
+        res = torch.zeros(conf_mat.shape[0], dtype=torch.float32, device=torch.cuda.current_device())
         for i in range(conf_mat.shape[0]):
             TP = conf_mat[i, i]
-            TP_FP = np.sum(conf_mat[:, i])
+            TP_FP = conf_mat[:, i].sum()
             res[i] = TP / TP_FP
         return res
 
     @staticmethod
-    def recall(conf_mat: np.ndarray):
-        res = np.zeros(conf_mat.shape[0], dtype=np.float32)
+    def recall(conf_mat: Tensor):
+        res = torch.zeros(conf_mat.shape[0], dtype=torch.float32, device=torch.cuda.current_device())
         for i in range(conf_mat.shape[0]):
             TP = conf_mat[i, i]
-            TP_FN = np.sum(conf_mat[i, :])
+            TP_FN = conf_mat[i, :].sum()
             res[i] = TP / TP_FN
         return res
 
+class Recorder:
+    def __init__(self, num_classes: int, k: int = 5):
+        self.device = torch.cuda.current_device()
+        self.loss: list = []
+        self.conf_mat: Tensor = torch.zeros((num_classes, num_classes), dtype=torch.int32, device=self.device)
+        self.topk: list = []
+        self.k: int = k
+        
+        self.mean_loss = None
+        self.mean_topk = None
+        self.is_converged = False
+
+    def __call__(self, output: Tensor, label: Tensor, loss: Tensor):
+        self.loss.append(loss.item())
+        self.topk.append(Calculate.topk_accuracy(output, label, self.k))
+        Calculate.update_conf_mat(self.conf_mat, output, label)
+        
+    def converge(self):
+        '''
+        This method should be only called ONCE for converging data from all devices
+        '''
+        assert not self.is_converged, "recorder is already converged"
+        self.mean_loss = sum(self.loss) / len(self.loss)
+        self.mean_topk = sum(self.topk) / len(self.topk)
+        if not dist.is_initialized():
+            return
+        mean_loss = Tensor([self.mean_loss], device=self.device)
+        mean_topk = Tensor([self.mean_topk], device=self.device)
+        dist.barrier()
+        dist.all_reduce(mean_loss, op=dist.ReduceOp.SUM)
+        dist.all_reduce(mean_topk, op=dist.ReduceOp.SUM)
+        dist.all_reduce(self.conf_mat, op=dist.ReduceOp.SUM)
+        self.mean_loss = (mean_loss / dist.get_world_size()).item()
+        self.mean_topk = (mean_topk / dist.get_world_size()).item()
+        self.is_converged = True
+
+    def get_mean_loss(self):
+        assert self.is_converged, "converge the recorder before get any vals"
+        return self.mean_loss
+    
+    def get_mean_topk_acc(self):
+        assert self.is_converged, "converge the recorder before get any vals"
+        return self.mean_topk
+        
+    def get_conf_mat(self):
+        assert self.is_converged, "converge the recorder before get any vals"
+        return self.conf_mat
+    
+    @staticmethod
+    def converge_loss(losses: List):
+        mean_loss = sum(losses) / len(losses)
+        if not dist.is_initialized():
+            return
+        mean_loss = Tensor([mean_loss], device=torch.cuda.current_device())
+        dist.barrier()
+        dist.all_reduce(mean_loss, op=dist.ReduceOp.SUM)
+        return (mean_loss / dist.get_world_size()).item()
+
 class IndexLogger:
 
-    class Recorder:
-        def __init__(self, num_classes: int, k: int = 5):
-            self.loss = []
-            self.conf_mat = np.zeros((num_classes, num_classes), dtype=np.uint32)
-            self.topk = []
-            self.k = k
-
-        def __call__(self, output: Tensor, label: Tensor, loss: Tensor):
-            self.loss.append(loss.item())
-            Calculate.update_conf_mat(self.conf_mat, output, label)
-            Calculate.topk_accuracy(output, label, self.k)
-
-        def get_mean_loss(self):
-            return sum(self.loss) / len(self.loss)
+    class Index:
+        def __init__(self, *args, **kwargs):
+            self.epoch = kwargs.get('epoch')
+            self.time = kwargs.get('time')
+            
+        def filled(self):
+            return all(v is not None for v in vars(self).values())
         
-        def get_mean_topk_acc(self):
-            return sum(self.topk) / len(self.topk)
-    
-    classify_indexes_init = {
-        'train_loss': [],
-        'val_loss': [],
-        'top1': [],
-        'top5': [],
-        'precision': [],
-        'recall': [],
-    }
-    
-    detect_indexes_init = {
-        'train_box_loss': [],
-        'train_cls_loss': [],
-        'train_dfl_loss': [],
-        'val_dfl_loss': [],
-        'val_dfl_loss': [],
-        'val_dfl_loss': [],
-        'precision': [],
-        'recall': [],
-        'mAP50': [],
-        'mAP50-95': [],
-    }
+        def record_train(self, loss):
+            raise NotImplementedError
+        
+        def record_val(self, recorder: Recorder):
+            raise NotImplementedError
+        
+    class ClassifyIndex(Index):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.train_loss = kwargs.get('train_loss')
+            self.val_loss = kwargs.get('val_loss')
+            self.top1_acc = kwargs.get('top1_acc')
+            self.top5_acc = kwargs.get('top5_acc')
+            self.precision = kwargs.get('precision')
+            self.recall = kwargs.get('recall')
+            
+        def record_train(self, loss):
+            self.train_loss = loss
+            
+        def record_val(self, recorder: Recorder):
+            self.val_loss = recorder.get_mean_loss()
+            self.top5_acc = recorder.get_mean_topk_acc()
+            self.top1_acc = Calculate.top1_accuracy(recorder.get_conf_mat())
+            self.precision = Calculate.precision(recorder.get_conf_mat())
+            self.recall = Calculate.recall(recorder.get_conf_mat())
+            
+            
+    class DetectIndex(Index):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.train_box_loss = kwargs.get("train_box_loss")
+            self.train_cls_loss = kwargs.get("train_cls_loss")
+            self.train_dfl_loss = kwargs.get("train_dfl_loss")
+            self.val_dfl_loss = kwargs.get("val_dfl_loss")
+            self.val_dfl_loss = kwargs.get("val_dfl_loss")
+            self.val_dfl_loss = kwargs.get("val_dfl_loss")
+            self.precision = kwargs.get("precision")
+            self.recall = kwargs.get("recall")
+            self.mAP50 = kwargs.get("mAP50")
+            self.mAP50_95 = kwargs.get("mAP50_95")
 
     def __init__(self, cd: CommandDetails, model_desc: dict):
-        self.timer = None
+        self.start_time = None
         self.cd = cd
         self.model_desc = model_desc
-        self.indexes: Dict[str: List] = {
-            'epoch': [],
-            'time': [],
-        }
-        if cd.task == 'classify':
-            self.indexes.update(IndexLogger.classify_indexes_init)
-        elif cd.task == 'detect':
-            self.indexes.update(IndexLogger.detect_indexes_init)
+        self.indexes: List[IndexLogger.Index] = []
+        
+    def __call__(self, index: Index):
+        assert index.filled(), f"logger received an unfilled index"
+        self.indexes.append(index)
 
     def start(self):
-        self.timer = time.time()
-
-    def update(self, *args, **kwargs):
-        for k, v in kwargs.items():
-            assert isinstance(self.indexes[k], list)
-            self.indexes[k].append(v)
-
-    def record_classify_epoch_result(self, recoder: Recorder):
-        num_classes = self.model_desc.get("nc")  # Get Number of Classes
-
-        self.indexes['val_loss'].append(recoder.get_mean_loss())
-        self.indexes['top5'].append(recoder.get_mean_topk_acc())
-        self.indexes['top1'].append(Calculate.top1_accuracy(recoder.conf_mat))
-        self.indexes['precision'].append(Calculate.precision(recoder.conf_mat))
-        self.indexes['recall'].append(Calculate.recall(recoder.conf_mat))
+        self.start_time = time.time()
+        
+    def get_time(self) -> float:
+        return time.time() - self.start_time
         
